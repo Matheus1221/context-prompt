712d9526e98de27e55cf543a2066269d9c5c1965
Matheus Quon Felix <58350861+Matheus1221@users.noreply.github.com>
2026-03-12 00:19:00 -0300

    feat: initial commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import styles from "./Home.module.scss";
+import Button from "@/components/ui/button";
 
 export function Home() {
   return (
     <div className={styles.page}>
       <main>
-        <h1>Olá Mundo Porra</h1>
-        <button></button>
+        <h1 className="text-4xl font-bold text-blue-500">Tailwind</h1>
+        <Button>Click me</Button>
       </main>
     </div>
   );
