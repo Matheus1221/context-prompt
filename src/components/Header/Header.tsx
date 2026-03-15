3b4f2e8054c28ab94a575a5aeedde6670f6105bc
Matheus Quon Felix <58350861+Matheus1221@users.noreply.github.com>
2026-03-15 02:31:10 -0300

    feat: refactor Header component and add Input component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,17 @@
-import { ArrowLeftIcon } from "@radix-ui/react-icons";
+import brand from "../../assets/brand-image.png";
+import { Input } from "../ui/input";
 
 export function Header() {
   return (
-    <header>
-      <div className="topBar">
-        <div className="topItem">
-          <span>
-            <ArrowLeftIcon />
-          </span>
+    <header className="w-full max-[1200px] mx-auto bg-transparent flex items-center justify-between p-4">
+      <div className="flex items-center gap-4">
+        <div className="">
+          <img className="h-24" src={brand} alt="Brand Logo" />
         </div>
+        <div>
+          <Input placeholder="Search"></Input>
+        </div>
+        <h1 className="text-2xl font-bold bg-black">Header</h1>
       </div>
     </header>
   );
